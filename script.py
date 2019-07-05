633df04051b29a637e01ac7d429f55d674225c53
ryaningham2001 <ryaningham2001@gmail.com>
2019-07-05 16:21:32 +0700

    dir cleanup

--- script.py
+++ script.py
@@ -2,12 +2,15 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import shutil
 from datetime import datetime
+import os
 
 TIME = datetime.now()
 
+# authenticate google drive API
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile("creds.txt")
 
+# create new creds.txt so manual auth not required every time
 if not gauth.credentials:
     gauth.LocalWebserverAuth()
 
@@ -18,11 +21,20 @@ else:
 
 gauth.SaveCredentialsFile("creds.txt")
 
+# initialise drive file
 drive = GoogleDrive(gauth)
 new_file_name = "emrbackup_{}".format(TIME)
-shutil.make_archive(new_file_name, 'zip', 'xampp')
+
+# create a copy of the xampp folder for permission reasons 
+shutil.copytree('xampp', 'xampp_copy')
+shutil.make_archive(new_file_name, 'zip', 'xampp_copy')
+
+# upload file
 xampp_archive = drive.CreateFile(metadata={'title': new_file_name})
 xampp_archive.Upload()
 
+# clean up directory
+shutil.rmtree('xampp_copy')
+os.remove('emrbackup_{}'.format(TIME))
 
 
