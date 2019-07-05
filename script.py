9b6c3a8244ce46664ec4b036c2ae08414a9b3af0
ryaningham2001 <ryaningham2001@gmail.com>
2019-07-05 15:55:45 +0700

    first commit

---
+++ script.py
@@ -0,0 +1,27 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import shutil
+from datetime import datetime
+
+TIME = datetime.now()
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("creds.txt")
+
+if not gauth.credentials:
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+
+gauth.SaveCredentialsFile("creds.txt")
+
+drive = GoogleDrive(gauth)
+new_file_name = "emrbackup_{}".format(TIME)
+shutil.make_archive(new_file_name, 'zip', 'xampp')
+
+
+
+
