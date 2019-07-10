b1ebda48c857e176ccf676fb939ada1c8bfd96d1
ryaningham2001 <ryaningham2001@gmail.com>
2019-07-10 14:20:13 +0700

    added logging for easy debugging

--- script.py
+++ script.py
@@ -6,19 +6,25 @@ import os
 
 TIME = datetime.now()
 
+print("EMR Backup script.\nDo not close this script if manually backing up.\nIf there are any errors, contact Ryan at ryaningham2001@gmail.com\n or see the GitHub repo at https://www.github.com/RyanIngham2001/emrbackup")
+
 # authenticate google drive API
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile("creds.txt")
 
 # create new creds.txt so manual auth not required every time
 if not gauth.credentials:
+    print("Unrecognised device. Please authorise on Google.")
     gauth.LocalWebserverAuth()
 
 elif gauth.access_token_expired:
+    print("Credentials for this device have expired.")
     gauth.Refresh()
 else:
+    print("Valid authorisation credentials.")
     gauth.Authorize()
 
+print("Saving OAuth credentials...")
 gauth.SaveCredentialsFile("creds.txt")
 
 # initialise drive file
@@ -27,7 +33,9 @@ new_file_name = "emrbackup_{}".format(TIME)
 
 # create a copy of the xampp folder for permission reasons 
 try:
+    print("Copying Xampp folder. This may take some time...")
     shutil.copytree('xampp', 'xampp_copy')
+    print("Creating archive. This may take some time...")
     shutil.make_archive(new_file_name, 'zip', 'xampp_copy')
 except FileNotFoundError:
     print("ERROR: Xampp folder is not found. Please make sure this script is executing in the same directory as Xampp.\nPress ENTER to quit.")
@@ -36,7 +44,9 @@ except FileNotFoundError:
 
 # upload file
 xampp_archive = drive.CreateFile(metadata={'title': new_file_name})
+print("Uploading archive...")
 xampp_archive.Upload()
+print("EMR has successfully been backed up to Google Drive.")
 
 # clean up directory
 shutil.rmtree('xampp_copy')
